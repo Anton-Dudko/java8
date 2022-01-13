0dd3c5a7616b04015ed8a33c4377cc12e11c1e55
Anton-Dudko <60478086+Anton-Dudko@users.noreply.github.com>
2022-01-13 20:59:46 +0300

    second commit

---
+++ java/src/lab/StrongTrial.java
@@ -0,0 +1,16 @@
+package lab;
+
+public class StrongTrial extends Trial{
+    private static final int MARK1_COEFF = 2;
+    public  StrongTrial(){
+    }
+    public StrongTrial(String name, int mark1, int mark2){
+        super(name, mark1, mark2);
+    }
+    public int result() {
+        return getMark1() / MARK1_COEFF + getMark2();
+    }
+
+
+
+}
